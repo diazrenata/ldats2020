f99c1ad67454ba263ad299bd0767ddbba09bb1e0
Renata Diaz <diaz.renatam@gmail.com>
2020-10-02 15:58:11 -0400

    test subset

--- crossval_tests.R
+++ crossval_tests.R
@@ -24,3 +24,49 @@ test_that("portal dat is correct", {
   expect_true(max(abund_dat$covariates$year) == 2018)
   
   })
+
+test_that("subset one works", {
+  
+  y1_subset <- subset_data_one(abund_dat, 1, 2)
+  
+  expect_true(length(y1_subset) == 5)
+  
+  expect_true(nrow(y1_subset$train$abundance) == 
+                length(y1_subset$train$covariates))
+  
+  expect_true(nrow(y1_subset$train$abundance) == 
+                (nrow(y1_subset$full$abundance) - 3))
+  
+  expect_true(all(y1_subset$test$abundance[1, ] == y1_subset$full$abundance[1, ]))
+  
+  expect_true(all(y1_subset$train$abundance == y1_subset$full$abundance[4:40, ]))
+  
+  expect_true(all(y1_subset$test$covariates[1] == y1_subset$full$covariates$year[1]))
+  
+  expect_true(all(y1_subset$train$covariates == y1_subset$full$covariates[4:40, ]))
+  
+  y5_subset <- subset_data_one(abund_dat, 5, 2)
+  
+  expect_true(length(y5_subset) == 5)
+  
+  expect_true(nrow(y5_subset$train$abundance) == 
+                length(y5_subset$train$covariates))
+  
+  expect_true(nrow(y5_subset$train$abundance) == 
+                (nrow(y5_subset$full$abundance) - 5))
+  
+  expect_true(all(y5_subset$test$abundance[1, ] == y5_subset$full$abundance[5, ]))
+  
+  expect_true(all(y5_subset$train$abundance == y5_subset$full$abundance[c(1:2, 8:40), ]))
+  
+  expect_true(all(y5_subset$test$covariates[1] == y5_subset$full$covariates$year[5]))
+  
+  expect_true(all(y5_subset$train$covariates == y5_subset$full$covariates[c(1:2, 8:40), ]))
+  
+  
+})
+
+test_that("subset all works", {
+  
+  
+})
