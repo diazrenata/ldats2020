f60817524298a46424e2c61b3fae53ba3d121c38
Renata Diaz <diaz.renatam@gmail.com>
2021-05-22 16:42:47 -0400

    routes downloaded from sciencebase may 22 2021

--- analysis/model_summary_dev.R
+++ analysis/model_summary_dev.R
@@ -2,20 +2,19 @@ library(drake)
 library(LDATS)
 #remotes::install_github("diazrenata/cvlt")
 library(cvlt)
-
+library(ggplot2)
 
 ## Set up the cache and config
 db <- DBI::dbConnect(RSQLite::SQLite(), here::here("analysis", "drake", "drake-cache-cvlt.sqlite"))
 cache <- storr::storr_dbi("datatable", "keystable", db)
 cache$del(key = "lock", namespace = "session")
 
-loadd(best_config_all_dataset_fits_bbs_rtrg_102_18, cache = cache)
-loadd(bbs_rtrg_102_18, cache = cache)
+loadd(best_config_all_dataset_fits_portal_annual, cache = cache)
+loadd(portal_annual, cache = cache)
 
-best_config_modified <- best_config_all_dataset_fits_bbs_rtrg_102_18 %>%
-  dplyr::mutate(cpts = 2)
-
-best_mod <- run_best_model(bbs_rtrg_102_18, best_config_modified, nit = 10000)
+best_config_modified <- best_config_all_dataset_fits_portal_annual %>%  dplyr::mutate(cpts = 3)
+best_config <- best_config_all_dataset_fits_portal_annual
+best_mod <- run_best_model(portal_annual, best_config_modified, nit = 1000)
 
 
 
@@ -56,8 +55,9 @@ modal_rhos <- common_rhos %>%
   dplyr::filter(tally == max(common_rhos$tally)) %>%
   dplyr::select(-tally, -draw) %>%
   dplyr::distinct()
-
+# 
 modal_ts <- LDATS::multinom_TS(data = best_mod$ts_mod$data, best_mod$ts_mod$formula, changepoints = unlist(modal_rhos[1,]), timename = "year")
+#modal_ts <- LDATS::multinom_TS(data = best_mod$ts_mod$data, best_mod$ts_mod$formula, changepoints = NULL, timename = "year")
 
 modal_ts_fits <- list()
 
@@ -85,19 +85,52 @@ fitted_data <- modal_abundance_preds %>%
   as.data.frame()
 colnames(fitted_data) <- colnames(best_mod$dataset$abundance)
 
-fitted_data <- fitted_data %>%
-  dplyr::mutate(year = best_mod$dataset$covariates$year)
 
-fitted_data_long <- tidyr::pivot_longer(fitted_data, -c(year), names_to = "species", values_to = "fitted_prop")
 
-actual_data <- (best_mod$dataset$abundance) 
+## use fitted values to calculate magnitude of cpt change ##
+
+segment_fitted_values <- fitted_data %>%
+  dplyr::mutate(year = best_mod$dataset$covariates$year) %>%
+  dplyr::left_join(dplyr::select(modal_ts_fits, year, seg)) %>%
+  dplyr::select(-year) %>%
+  dplyr::distinct()
+
+segment_matrix <- segment_fitted_values %>%
+  dplyr::select(-seg) %>%
+  as.matrix()
+
+segment_bc <- as.matrix(vegan::vegdist(segment_matrix, method = "bray"))
+
+changepoint_distance <- cpt_summary %>%
+  dplyr::mutate(seg_before = as.numeric(cpt),
+                seg_after = as.numeric(cpt) + 1,
+                dissimilarity = NA)
+
+for(i in 1:nrow(changepoint_distance)) {
+  changepoint_distance$dissimilarity[i] <- segment_bc[changepoint_distance$seg_before[i], changepoint_distance$seg_after[i]]
+}
+
+## use fitted and actual values to calculate various r2s ## 
+
+actual_data <- (best_mod$dataset$abundance) %>%
+  dplyr::mutate_all(as.numeric)
 
 for(i in 1:nrow(actual_data)) {
   actual_data[i,] <- actual_data[i,] / sum(actual_data[i,])
 }
 
+# 
+# for(i in 1:nrow(fitted_data)) {
+#   fitted_data[i, ] <- fitted_data[i, ] * sum(actual_data[i, ])
+# }
+
+fitted_data <- fitted_data %>%
+  dplyr::mutate(year = best_mod$dataset$covariates$year)
+
 actual_data <- as.data.frame(actual_data) %>%
   dplyr::mutate(year = best_mod$dataset$covariates$year)
+
+fitted_data_long <- tidyr::pivot_longer(fitted_data, -c(year), names_to = "species", values_to = "fitted_prop")
 
 actual_data_long <- tidyr::pivot_longer(actual_data, -c(year), names_to = "species", values_to = "actual_prop")
 
@@ -106,6 +139,16 @@ fitted_actual <- dplyr::left_join(fitted_data_long, actual_data_long)
 ggplot(fitted_actual, aes(actual_prop, fitted_prop)) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0)
+
+
+
+ggplot(fitted_actual, aes(actual_prop, fitted_prop, color = species)) +
+  geom_point() +
+  stat_ellipse() +
+  geom_abline(slope = 1, intercept = 0) +
+  theme(legend.position = "none") +
+  scale_color_viridis_d(option = "mako")
+
 
 ggplot(fitted_actual, aes(actual_prop, fitted_prop)) +
   geom_point() +
